7e8473a26bf727c8a73d39f83486376581c2f25a
Brandon Loveall <bloveall02@gmail.com>
2026-06-01 20:12:08 -0600

    playcard remote event updated to translate card_id to playable card

--- src/shared/remotes/PlayCard/Interface.ts
+++ src/shared/remotes/PlayCard/Interface.ts
@@ -1,4 +1,6 @@
 import { ReplicatedStorage } from "@rbxts/services";
+import { CardRegistry } from "shared/card_registry";
+import { CostingCard } from "shared/objects/abstracts/card_inherits/player_card_inherits/costing_card";
 
 const PlayCard = ReplicatedStorage.WaitForChild("TS").WaitForChild("remotes").WaitForChild("PlayCard").WaitForChild("PlayCard") as RemoteEvent
 
@@ -6,6 +8,6 @@ export function PlayCard_Pub(card_id: string) {
     PlayCard.FireServer(card_id);
 }
 
-export function PlayCard_Sub(callback: (card_id: string) => void) {
-    PlayCard.OnClientEvent.Connect(callback)
+export function PlayCard_Sub(callback: (player: Player, card: CostingCard) => void) {
+    PlayCard.OnClientEvent.Connect((plr: Player, card_id: string) => { callback(plr, CardRegistry.get(card_id) as CostingCard) })
 }
