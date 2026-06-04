246d81441d7f3d32e227308a0090f60292c0a461
Brandon Loveall <bloveall02@gmail.com>
2026-06-04 13:47:06 -0600

    gamecontext updated

--- src/shared/game_context.ts
+++ src/shared/game_context.ts
@@ -1,4 +1,11 @@
+import { ActCard } from "./objects/abstracts/card_inherits/nonplayer_card_inherits/story_card_inherits/act_card";
+import { AgendaCard } from "./objects/abstracts/card_inherits/nonplayer_card_inherits/story_card_inherits/agenda_card";
+import { LocationCard } from "./objects/abstracts/card_inherits/nonplayer_card_inherits/story_card_inherits/location_card";
+import { ChaosBag } from "./objects/chaos_bag";
+import { Deck } from "./objects/deck";
 import { GamePlayer } from "./objects/player";
+import { _01105 } from "./objects/tangible_cards/01105";
+import { _01108 } from "./objects/tangible_cards/01108";
 
 export enum GameState {
     NONE,
@@ -8,12 +15,36 @@ export enum GameState {
     Playerturn_Ended,
 }
 
-interface GameContext {
-    current_game_state: GameState,
-    players: GamePlayer[]
-}
+let dudAgenda = new _01105();
+let dudAct = new _01108()
+let dudBag = new ChaosBag([0]);
+let dudDeck = new Deck([]);
 
-export const GameContext = {
+export const GameContext: {
+    current_game_state: GameState,
+    players: GamePlayer[],
+    agenda: AgendaCard,
+    act: ActCard,
+    chaos_bag: ChaosBag,
+    encounter_deck: Deck,
+    game_map: (LocationCard | undefined)[][]
+} = {
     current_game_state: GameState.NONE,
     players: [],
-} satisfies GameContext
+    agenda: dudAgenda,
+    act: dudAct,
+    chaos_bag: dudBag,
+    encounter_deck: dudDeck,
+    game_map: [ // Hard coded to be a 10x10 grid. Array.from not available in Luau, so have to hard code it
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
+]
+}
