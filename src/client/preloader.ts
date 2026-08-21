d74620a48a718d2cc8c3eeec5ba0af64d9286bce
Brandon Loveall <bloveall02@gmail.com>
2026-08-21 13:46:01 -0600

    removed counter debugger

--- src/client/preloader.ts
+++ src/client/preloader.ts
@@ -4,10 +4,6 @@ const ui = Players.LocalPlayer.WaitForChild("PlayerGui");
 
 const allUi = ui.GetDescendants();
 
-let counter = 0;
-
 for (const item of allUi) {
 	ContentProvider.PreloadAsync([item]);
-	counter++;
-	print(counter);
 }
